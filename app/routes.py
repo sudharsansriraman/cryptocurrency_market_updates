2df7b1ccb66f282446d8256f96c933964c0fe4d3
sudharsansriraman <s.sudhar95@gmail.com>
2023-10-24 00:55:50 +0530

    Added a new feature for market data retrieval

--- app/routes.py
+++ app/routes.py
@@ -7,14 +7,20 @@ api = Blueprint('api', __name__)
 @api.route('/market_summaries', methods=['GET'])
 def get_all_market_summaries():
     url = f"{BITTREX_API_BASE_URL}/markets/summaries"
-    response = requests.get(url)
-    data = response.json()
-    return jsonify(data)
+    try:
+        response = requests.get(url)
+        data = response.json()
+        return jsonify(data)
+    except requests.exceptions.RequestException as e:
+        return jsonify({'error': str(e)}), 500
 
 @api.route('/market_summary/<market>', methods=['GET'])
 def get_market_summary(market):
     market_symbol = market
     url = f"{BITTREX_API_BASE_URL}/markets/{market_symbol}/summary"
-    response = requests.get(url)
-    data = response.json()
-    return jsonify(data)
+    try:
+        response = requests.get(url)
+        data = response.json()
+        return jsonify(data)
+    except requests.exceptions.RequestException as e:
+        return jsonify({'error': str(e)}), 500
